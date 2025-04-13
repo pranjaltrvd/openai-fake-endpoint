3523b49bc2971dc7abe001252038dae5e25fb952
Pranjal  Trivedi <pranjaltrivedi@MacBook-Pro-2.local>
2025-04-13 14:01:25 +0530

    add random sleep for each req

--- main.py
+++ main.py
@@ -63,10 +63,9 @@ def data_generator():
 @app.post("/v1/chat/completions")
 @app.post("/openai/deployments/{model:path}/chat/completions")  # azure compatible endpoint
 async def completion(request: Request):
-    _time_to_sleep = os.getenv("TIME_TO_SLEEP", None)
-    if _time_to_sleep is not None:
-        print("sleeping for " + _time_to_sleep)
-        await asyncio.sleep(float(_time_to_sleep))
+    random_sleep = random.randint(5, 10)
+    print(f"additional random sleep for {random_sleep} seconds")
+    await asyncio.sleep(random_sleep)
 
     data = await request.json()
 
